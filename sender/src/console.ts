1d88c3f248023a3105c9cf2419c38634e6078e23
kaddo <kofi.addo@facile.it>
2020-01-01 19:26:07 +0100

    fix: typo

---
+++ sender/src/console.ts
@@ -0,0 +1,12 @@
+import commander from "commander";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+commander.version("beta").description("i send stuff to rabbit");
+commander.command("send")
+    .alias("s")
+    .description("am the sender")
+    .action(() => {
+        console.log("have send it")
+    });
