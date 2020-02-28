9afb25c1e672ab2cca4ec166c9a7d19ff09c5a69
kaddo <kofi.addo@facile.it>
2020-02-28 21:39:53 +0100

    feat: console lint and prettier

--- receiver/src/console.ts
+++ receiver/src/console.ts
@@ -1,10 +1,11 @@
 import commander from "commander";
 
 commander.version("beta").description("i receive stuff");
-commander.command("receive")
-    .alias("r")
-    .description("am the receiver")
-    .action(() => {
-        console.log("i have receive it")
-    });
+commander
+  .command("receive")
+  .alias("r")
+  .description("am the receiver")
+  .action(() => {
+    console.log("i have receive it");
+  });
 commander.parse(process.argv);
