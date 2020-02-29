dd732f4846df9ffe8b504fd205215e046d1e4641
kaddo <kofi.addo@facile.it>
2020-02-29 22:17:16 +0100

    feat: new implementation

--- sender/config/Send.ts
+++ sender/config/Send.ts
@@ -1,54 +1,45 @@
-import amqp from "amqplib/callback_api";
-import { ClientHttp2Session } from "http2";
+import amqp from "amqp-ts";
 import { SendConfigInterface } from "./SendConfigInterface";
 
 require("dotenv").config();
 
 export class Send implements SendConfigInterface {
   connect(): void {
-    amqp.connect(
+    const connection = new amqp.Connection(
+      `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}${process.env.RABBITMQ_VHOST}`
+    );
+    const exchange = connection.declareExchange(
+      process.env.RABBITMQ_WORKER_EXCHANGE,
+      "fanout",
       {
-        hostname: process.env.RABBITMQ_HOST,
-        port: parseInt(process.env.RABBITMQ_PORT),
-        username: process.env.RABBITMQ_USER,
-        password: process.env.RABBITMQ_PASSWORD,
-        vhost: process.env.RABBITMQ_VHOST
-      },
-      (err: ClientHttp2Session, conn) => {
-        conn.createChannel((err, ch) => {
-          // set exchange that is being used
-          ch.assertExchange(process.env.RABBITMQ_WORKER_EXCHANGE, "fanout", {
-            durable: true,
-            autoDelete: false,
-            arguments: null
-          });
-          // set queue that is being used
-          ch.assertQueue(
-            process.env.RABBITMQ_QUEUE,
-            { durable: true, exclusive: false, autoDelete: false, arguments: null },
-            (err, q) => {
-              console.log(
-                " [*] Waiting for messages in %s. To exit press CTRL+C",
-                q.queue
-              );
-              // bind the queue to the exchange
-              ch.bindQueue(q.queue, process.env.RABBITMQ_WORKER_EXCHANGE, "");
-              // consume from the queue, one message at a time.
-              ch.consume(
-                q.queue,
-                function(msg) {
-                  console.log("Message received: %s", msg.content.toString());
-                  //save message to db
-                  //acknowledge receipt of message to amqp
-                  console.log("Acknowledging message");
-                  ch.ack(msg, true);
-                },
-                { noAck: false }
-              );
-            }
-          );
-        });
+        durable: true,
+        autoDelete: false,
+        arguments: null
       }
     );
+
+    const queue1 = connection.declareQueue(process.env.RABBITMQ_QUEUE_ONE, {
+      durable: true,
+      exclusive: false,
+      autoDelete: false,
+      arguments: null
+    });
+
+    const queue2 = connection.declareQueue(process.env.RABBITMQ_QUEUE_SEC, {
+      durable: true,
+      exclusive: false,
+      autoDelete: false,
+      arguments: null
+    });
+
+    queue1.bind(exchange);
+    queue2.bind(exchange);
+
+    connection.completeConfiguration().then(() => {
+      const message = new amqp.Message("Hello queues");
+      exchange.send(message);
+    });
+
+    connection.close().then(console.log("Closing channel"));
   }
 }
