128d51afd53a56345b12cf5fbb18c4ee025398d2
kaddo <kofi.addo@facile.it>
2020-03-01 22:41:29 +0100

    feat: types

--- sender/console.ts
+++ sender/console.ts
@@ -1,6 +1,7 @@
 import commander from "commander";
 import dotenv from "dotenv";
 import { Send } from "./config/Send";
+import { SendConfigInterface } from "./config/SendConfigInterface";
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ commander
   .alias("s")
   .description("am the sender")
   .action(() => {
-    let sender;
+    let sender: SendConfigInterface;
     sender = new Send();
     sender.connect();
   });
