e9ae3ae663d2853feae6fd408998f2ef7abab841
kaddo <kofi.addo@facile.it>
2020-03-09 20:45:04 +0100

    feat: async and await receive

--- receiver/config/Receive.ts
+++ receiver/config/Receive.ts
@@ -1,46 +1,33 @@
+import amqp, {Channel, Connection, ConsumeMessage} from "amqplib";
 import { ReceiverInterface } from "./ReceiverInterface";
-import amqp = require("amqplib/callback_api");
 
 require("dotenv").config();
 
 export class Receive implements ReceiverInterface {
-  receiver(): void {
-    amqp.connect(
+  async receiver(): Promise<void> {
+    let connection: Connection = await amqp.connect({
+      protocol: process.env.RABBITMQ_PROTOCOL,
+      hostname: process.env.RABBITMQ_HOST,
+      port: parseInt(process.env.RABBITMQ_PORT),
+      username: process.env.RABBITMQ_USER,
+      password: process.env.RABBITMQ_PASSWORD,
+      vhost: process.env.RABBITMQ_VHOST
+    });
+    let channel: Channel = await connection.createChannel();
+    const queueAssert = await channel.assertQueue(
+      process.env.RABBITMQ_QUEUE_ONE,
       {
-        protocol: process.env.RABBITMQ_PROTOCOL,
-        hostname: process.env.RABBITMQ_HOST,
-        port: parseInt(process.env.RABBITMQ_PORT),
-        username: process.env.RABBITMQ_USER,
-        password: process.env.RABBITMQ_PASSWORD,
-        vhost: process.env.RABBITMQ_VHOST
-      },
-      (err, conn) => {
-        if (err) {
-          throw err;
-        }
-        conn.createChannel((err, ch) => {
-          if (err) {
-            throw err;
-          }
-          ch.assertQueue(process.env.RABBITMQ_QUEUE_ONE, {
-            durable: true,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null
-          });
-          console.log(
-            " [*] Waiting for messages in %s. To exit press CTRL+C",
-            process.env.RABBITMQ_QUEUE_ONE
-          );
-          ch.consume(
-            process.env.RABBITMQ_QUEUE_ONE,
-            msg => {
-              console.log(" [x] Received %s", msg.content.toString());
-              ch.ack(msg, false);
-            },
-          );
-        });
+        durable: true,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
       }
     );
+    await channel.consume(queueAssert.queue, (msg: ConsumeMessage | null) => {
+      console.log(" [x] Received %s", msg.content.toString());
+      channel.ack(msg, false);
+    });
+    await channel.close();
+    await connection.close();
   }
 }
--- receiver/config/ReceiverInterface.ts
+++ receiver/config/ReceiverInterface.ts
@@ -1,3 +1,3 @@
 export interface ReceiverInterface {
-  receiver(): void;
+  receiver(): Promise<void>;
 }
