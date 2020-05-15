09b10a1ce7200ec3f97a97d16fda01a01519c640
kaddo <kofi.addo@facile.it>
2020-05-16 00:28:39 +0200

    feat: prettify

--- sender/config/Send.ts
+++ sender/config/Send.ts
@@ -1,62 +1,40 @@
-import amqp, { Channel, Connection } from "amqplib";
-import { SendConfigInterface } from "./SendConfigInterface";
-require("dotenv").config();
+import amqp, { Channel, Connection } from 'amqplib';
+import { SendConfigInterface } from './SendConfigInterface';
+require('dotenv').config();
 
 export class Send implements SendConfigInterface {
-  async publisher(message?: string): Promise<void> {
-    let connection: Connection = await amqp.connect({
-      protocol: process.env.RABBITMQ_PROTOCOL,
-      hostname: process.env.RABBITMQ_HOST,
-      port: parseInt(process.env.RABBITMQ_PORT),
-      username: process.env.RABBITMQ_USER,
-      password: process.env.RABBITMQ_PASSWORD,
-      vhost: process.env.RABBITMQ_VHOST
-    });
-    let channel: Channel = await connection.createChannel();
-    await channel.assertExchange(
-      process.env.RABBITMQ_WORKER_EXCHANGE,
-      "direct",
-      {
-        durable: true,
-        autoDelete: false,
-        arguments: null
-      }
-    );
-    const queueOne = await channel.assertQueue(
-      process.env.RABBITMQ_QUEUE_ONE,
-      {
-        durable: true,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null
-      }
-    );
-    const queueSec = await channel.assertQueue(
-      process.env.RABBITMQ_QUEUE_SEC,
-      {
-        durable: true,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null
-      }
-    );
-    await channel.bindQueue(
-      queueOne.queue,
-      process.env.RABBITMQ_WORKER_EXCHANGE,
-      "info"
-    );
+    async publisher(message?: string): Promise<void> {
+        const connection: Connection = await amqp.connect({
+            protocol: process.env.RABBITMQ_PROTOCOL,
+            hostname: process.env.RABBITMQ_HOST,
+            port: parseInt(process.env.RABBITMQ_PORT),
+            username: process.env.RABBITMQ_USER,
+            password: process.env.RABBITMQ_PASSWORD,
+            vhost: process.env.RABBITMQ_VHOST,
+        });
+        const channel: Channel = await connection.createChannel();
+        await channel.assertExchange(process.env.RABBITMQ_WORKER_EXCHANGE, 'direct', {
+            durable: true,
+            autoDelete: false,
+            arguments: null,
+        });
+        const queueOne = await channel.assertQueue(process.env.RABBITMQ_QUEUE_ONE, {
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null,
+        });
+        const queueSec = await channel.assertQueue(process.env.RABBITMQ_QUEUE_SEC, {
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null,
+        });
+        await channel.bindQueue(queueOne.queue, process.env.RABBITMQ_WORKER_EXCHANGE, 'info');
 
-    await channel.bindQueue(
-      queueSec.queue,
-      process.env.RABBITMQ_WORKER_EXCHANGE,
-      "warning"
-    );
-    await channel.publish(
-      process.env.RABBITMQ_WORKER_EXCHANGE,
-      "warning",
-      Buffer.from(message)
-    );
-    await channel.close();
-    await connection.close();
-  }
+        await channel.bindQueue(queueSec.queue, process.env.RABBITMQ_WORKER_EXCHANGE, 'warning');
+        await channel.publish(process.env.RABBITMQ_WORKER_EXCHANGE, 'warning', Buffer.from(message));
+        await channel.close();
+        await connection.close();
+    }
 }
--- sender/console.ts
+++ sender/console.ts
@@ -1,20 +1,19 @@
-import commander from "commander";
-import dotenv from "dotenv";
-import { Send } from "./config/Send";
-import { SendConfigInterface } from "./config/SendConfigInterface";
+import commander from 'commander';
+import dotenv from 'dotenv';
+import { Send } from './config/Send';
+import { SendConfigInterface } from './config/SendConfigInterface';
 
 dotenv.config();
 
-commander.version("beta").description("i send stuff to rabbit");
+commander.version('beta').description('i send stuff to rabbit');
 commander
-  .command("send")
-  .alias("s")
-  .arguments("<message>")
-  .description("am the sender")
-  .action(async (message: string) => {
-    let sender: SendConfigInterface;
-    sender = new Send();
-    await sender.publisher(message);
-    console.log(`[x] Send ${message}`);
-  });
+    .command('send')
+    .alias('s')
+    .arguments('<message>')
+    .description('am the sender')
+    .action(async (message: string) => {
+        const sender: SendConfigInterface = new Send();
+        await sender.publisher(message);
+        console.log(`[x] Send ${message}`);
+    });
 commander.parse(process.argv);
