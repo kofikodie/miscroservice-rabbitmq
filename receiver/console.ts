1c24e615d57a9e284653c79b9e3881f5e4ee8a46
kaddo <kofi.addo@facile.it>
2020-05-16 14:41:42 +0200

    fix: prettier

--- receiver/console.ts
+++ receiver/console.ts
@@ -1,13 +1,13 @@
-import commander from "commander";
-import { Receive } from "./config/Receive";
+import commander from 'commander';
+import { Receive } from './config/Receive';
 
-commander.version("beta").description("i receive stuff");
+commander.version('beta').description('i receive stuff');
 commander
-  .command("receive")
-  .alias("r")
-  .description("am the receiver")
-  .action(async () => {
-    await new Receive().receiver();
-    console.log("closing connection");
-  });
+    .command('receive')
+    .alias('r')
+    .description('am the receiver')
+    .action(async () => {
+        await new Receive().receiver();
+        console.log('closing connection');
+    });
 commander.parse(process.argv);
