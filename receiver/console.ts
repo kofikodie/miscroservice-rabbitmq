f99050a33251a8e2b0de938c363aea2d55345a3f
kaddo <kofi.addo@facile.it>
2020-05-16 14:44:22 +0200

    feat: prettier

--- receiver/console.ts
+++ receiver/console.ts
@@ -1,17 +1,17 @@
-import commander from "commander";
-import {Receiver} from "./config/Receiver";
+import commander from 'commander';
+import { Receiver } from './config/Receiver';
 
-commander.version("beta").description("i receive stuff");
+commander.version('beta').description('i receive stuff');
 commander
-  .command("receive")
-  .alias("r")
-  .description("am the receiver")
-  .action( async () => {
-      const receive = new Receiver();
-      try {
-          await receive.receiver();
-      } catch (error) {
-          console.log(`[x] Message could not be delivered ${error}`);
-      }
-  });
+    .command('receive')
+    .alias('r')
+    .description('am the receiver')
+    .action(async () => {
+        const receive = new Receiver();
+        try {
+            await receive.receiver();
+        } catch (error) {
+            console.log(`[x] Message could not be delivered ${error}`);
+        }
+    });
 commander.parse(process.argv);
