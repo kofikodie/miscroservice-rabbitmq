6f892ea924645d0734a4d0033b83204bc68ba78f
kaddo <kofi.addo@facile.it>
2020-05-19 19:56:54 +0200

    feat: msg publisher

---
+++ sender/config/Send.ts
@@ -0,0 +1,57 @@
+import { SendConfigInterface } from './SenderInterface';
+import amqp, { Channel, Connection } from 'amqplib';
+import Env from './EnvRabbit';
+
+export class Send implements SendConfigInterface {
+    async publisher(message: string): Promise<void> {
+        const connection: Connection = await amqp.connect({
+            protocol: Env.PROTOCOL,
+            hostname: Env.HOST,
+            port: parseInt(Env.PORT),
+            username: Env.USER,
+            password: Env.PASSWORD,
+            vhost: Env.VHOST,
+        });
+
+        const channel: Channel = await connection.createChannel();
+        await channel.assertExchange(Env.WORKER_EXCHANGE, 'headers', {
+            durable: true,
+            autoDelete: false,
+            arguments: null,
+        });
+
+        const queueOne = await channel.assertQueue(Env.QUEUE_ONE, {
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null,
+        });
+        const queueSec = await channel.assertQueue(Env.QUEUE_SEC, {
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null,
+        });
+
+        await channel.bindQueue(queueOne.queue, Env.WORKER_EXCHANGE, '', {
+            'x-match': 'any',
+            job: 'convert',
+            format: 'jpep',
+        });
+        await channel.bindQueue(queueSec.queue, Env.WORKER_EXCHANGE, '', {
+            'x-match': 'all',
+            job: 'convert',
+            format: 'jpep',
+        });
+
+        channel.publish(Env.WORKER_EXCHANGE, '', Buffer.from(message), {
+            headers: {
+                job: 'convert',
+                format: 'bitmap',
+            },
+        });
+
+        await channel.close();
+        await connection.close();
+    }
+}
---
+++ sender/console.ts
@@ -0,0 +1,21 @@
+import commander from 'commander';
+import dotenv from 'dotenv';
+import { Send } from './config/Send';
+
+dotenv.config();
+
+commander.version('beta').description('i send stuff to rabbit');
+commander
+    .command('send')
+    .alias('s')
+    .arguments('<message>')
+    .description('am the sender')
+    .action(async (message: string) => {
+        try {
+            await new Send().publisher(message);
+            console.log(`[x] Send ${message}`);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+commander.parse(process.argv);
